fa4b7905e518a041af31a9431744bd02b8610885
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-14 15:59:26 -0300

    feat: obtain data for a single pokemon based on the pokedexNumber

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import db from './database/Mongo.js'
 import authRouter from "./routers/Auth.routes.js";
-import adminRouter from "./routers/Admin.routes.js"
+import adminRouter from "./routers/Admin.routes.js";
+import pokemonRouter from "./routers/Pokemon.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use(authRouter);
 app.use(adminRouter);
+app.use(pokemonRouter);
 
 const port = process.env.PORT;
 
