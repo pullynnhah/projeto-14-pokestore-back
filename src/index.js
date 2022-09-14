1bf233311b370b37af681e9091284d2501e40a18
wesley <wesleysantos20012001@gmail.com>
2022-09-14 12:48:42 -0300

    feat (signup/endpoint): implementation of signup endpoint => including: routers/Auth.routes.js and controllers/Auth.controllers.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
+import authRouter from "./routers/Auth.routes.js"; 
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
+app.use(authRouter);
+ 
 app.get("/test", (req, res) => {
   res.send("Hello World!");
 });
