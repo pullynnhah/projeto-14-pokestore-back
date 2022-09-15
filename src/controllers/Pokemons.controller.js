48586c85a9abc6a6f048c3075a54d479f166948a
Rodrigo <rodrigovf2102@gmail.com>
2022-09-15 15:22:03 -0300

    feat: in pokemonsRoutes added logic for getting legendary and other classes pokemons

--- src/controllers/Pokemons.controller.js
+++ src/controllers/Pokemons.controller.js
@@ -2,14 +2,23 @@ import db from "../database/Mongo.js";
 import { StatusCodes } from "http-status-codes";
 
 const getPokemons = async (req, res) => {
-    const filter = req.headers;
+    const filter = req.query;
     console.log(filter)
     try {
         let pokemons;
         if(filter.type === undefined || filter.type === 'all'){
             pokemons = await db.collection('Pokemons').find().toArray();
-        } else{
+        } 
+        if(filter.type !== 'others' && filter.type!=='all' && filter.type!==undefined && filter.type!=='legendary'){
             pokemons = await db.collection('Pokemons').find({$or:[{type1:filter.type},{type2:filter.type}]}).toArray();
+        }
+        if(filter.type === 'others'){
+            pokemons = await db.collection('Pokemons')
+            .find({$or:[{type1:{$in:['dark','flying','dragon','rock']}},{type2:{$in:['dark','flying','dragon','rock']}}]})
+            .toArray();
+        }
+        if(filter.type ==='legendary'){
+            pokemons = await db.collection('Pokemons').find({isLegendary:true}).toArray();
         }
         res.send(pokemons).status(StatusCodes.ACCEPTED);
     } catch (error) {
