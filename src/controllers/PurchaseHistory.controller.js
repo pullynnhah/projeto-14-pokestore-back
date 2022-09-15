18adc768eafc7a1b702d833d917a86627c993e2e
Rodrigo <rodrigovf2102@gmail.com>
2022-09-15 16:45:24 -0300

    feat: purchaseHistory routes and controller added

---
+++ src/controllers/PurchaseHistory.controller.js
@@ -0,0 +1,18 @@
+import db from "../database/Mongo.js";
+import { StatusCodes } from "http-status-codes";
+
+const getHistory = async (req,res) =>{
+    const {userid} = req.headers;
+    console.log(userid);
+    try {
+        let historyCartUser = await db.collection('Carts').find({userId:Number(userid)}).toArray();
+        historyCartUser = historyCartUser.filter(history=>history.purchaseDate!==false);
+        res.send(historyCartUser).status(StatusCodes.ACCEPTED); 
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+
+}
+
+export {getHistory}
