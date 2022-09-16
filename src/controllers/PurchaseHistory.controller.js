427f64b9e6753352c94d72786136ba63ad7a4f70
Rodrigo <rodrigovf2102@gmail.com>
2022-09-16 09:31:14 -0300

    feat: purchaseHistory routes and controller added

--- src/controllers/PurchaseHistory.controller.js
+++ src/controllers/PurchaseHistory.controller.js
@@ -6,13 +6,12 @@ const getHistory = async (req,res) =>{
     console.log(userid);
     try {
         let historyCartUser = await db.collection('Carts').find({userId:Number(userid)}).toArray();
-        historyCartUser = historyCartUser.filter(history=>history.purchaseDate!==false);
+        historyCartUser = historyCartUser.filter(history=>history.purchaseDate!==null);
         res.send(historyCartUser).status(StatusCodes.ACCEPTED); 
     } catch (error) {
         console.log(error.message);
         res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     }
-
 }
 
 export {getHistory}
